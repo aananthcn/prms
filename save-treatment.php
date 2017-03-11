8f32c492db75cff3f1633087f3cd6f8e1538035d
Aananth C N <c.n.aananth@gmail.com>
2017-03-11 19:05:40 +0530

    Eliminated single quotes in input texts for mysql

--- save-treatment.php
+++ save-treatment.php
@@ -18,6 +18,7 @@ function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
+  $data = str_replace("'", "", $data);
   return $data;
 }
 
